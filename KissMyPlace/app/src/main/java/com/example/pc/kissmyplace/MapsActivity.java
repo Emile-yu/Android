5a8e7c46b03671f349d3a1c20e44de018a5a3cab
Emile-yu <yjg9091@gmailc.om>
2018-01-23 18:31:40 +0100

    MapActivity
    MapActivity

--- KissMyPlace/app/src/main/java/com/example/pc/kissmyplace/MapsActivity.java
+++ KissMyPlace/app/src/main/java/com/example/pc/kissmyplace/MapsActivity.java
@@ -5,39 +5,51 @@ import android.content.DialogInterface;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+import java.util.List;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnMapClickListener {
 
     private GoogleMap mMap;
-    private LatLng start ;
+    LatLng start = new LatLng(54.001615, -2.794561);
+    List<LatLng> position = new ArrayList<LatLng>();
 
     int current = 0;
 
-    List<LatLng> ListCity = new ArrayList<LatLng>();
 
     private AlertDialog alert;
     private AlertDialog.Builder builder;
     private SupportStreetViewPanoramaFragment streetViewPanoramaFragment;
+
+    FragmentTransaction fragmentTransaction;
+    FragmentManager fragmentManager;
+
+
+    MapFragment mapFragment;
+    StreetFragment streetFragment;
 
 
     @Override
@@ -47,11 +59,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
 
+
+
         int lvl = getIntent().getIntExtra("lvl",0);
         if(lvl == 0){
-            ListCity.add(new LatLng(40.7248846, -73.98639679));
-            ListCity.add(new LatLng(39.70718666, -99.37133789));
-            ListCity.add(new LatLng(48.63290859, 2.26318359));
+            position.add(new LatLng(40.7248846, -73.98639679));
+            position.add(new LatLng(39.70718666, -99.37133789));
+            position.add(new LatLng(48.63290859, 2.26318359));
+
         }
 
         if(lvl == 1){
@@ -61,9 +76,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(lvl == 2){
             start = new LatLng(63.87365, 151.20689);
         }
-        start = ListCity.get(current);
 
-        streetViewPanoramaFragment =
+
+
+        MapFragment mapFragment = new MapFragment();
+        mapFragment.mapsActivity = this;
+
+        StreetFragment streetFragment = new StreetFragment();
+        streetFragment.mapsActivity = this;
+
+
+        fragmentManager = this.getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+
+
+        fragmentTransaction.add(R.id.map, (Fragment)mapFragment);
+        fragmentTransaction.add(R.id.streetviewpanorama, (Fragment)streetFragment);
+        fragmentTransaction.commit();
+
+       /* streetViewPanoramaFragment =
                 (SupportStreetViewPanoramaFragment) getSupportFragmentManager().findFragmentById(R.id.streetviewpanorama);
         streetViewPanoramaFragment.getStreetViewPanoramaAsync(
                 new OnStreetViewPanoramaReadyCallback() {
@@ -78,6 +110,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        */
     }
 
 
@@ -85,7 +118,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Setting a click event handler for the map
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+       /* mMap.setOnMapClickListener(new OnMapClickListener() {
 
             @Override
             public void onMapClick(final LatLng latLng) {
@@ -93,7 +126,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 MarkerOptions markerOptions = new MarkerOptions();
 
                 markerOptions.position(latLng);
-
                 markerOptions.title(latLng.latitude + " : " + latLng.longitude);
                 mMap.clear();
                 mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
@@ -103,40 +135,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 builder = new AlertDialog.Builder(MapsActivity.this);
                 alert = builder
                         .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setMessage("You are "+getDistance(ListCity.get(current),latLng)+"km away")
+                        .setMessage("You are "+getDistance(start,latLng)+"km away")
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                start = new LatLng(latLng.latitude, latLng.longitude);
 
-                                current++;
-                                if (current > ListCity.size() - 1) {
+                                streetViewPanoramaFragment.getStreetViewPanoramaAsync(
+                                        new OnStreetViewPanoramaReadyCallback() {
+                                            @Override
+                                            public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+                                                panorama.setPosition(latLng);
 
-                                    EndGame();
-                                } else {
-                                    streetViewPanoramaFragment.getStreetViewPanoramaAsync(
-                                            (panorama) -> {
-                                                panorama.setPosition(ListCity.get(current));
-                                            });
-                                }
-                            }
-
-
+                                            }
+                                        });                  }
                         })
-                        .create();
+
+                        .create();             //创建AlertDialog对象
                 alert.show();
 
             }
         });
 
+        */
     }
 
-    public void EndGame(){
 
-    }
-
-    public void CompteScore(LatLng latLng){
-
-    }
 
 
     public double getDistance(LatLng start,LatLng end){
@@ -146,14 +170,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         double lon1 = (Math.PI/180)*start.longitude;
         double lon2 = (Math.PI/180)*end.longitude;
 
+
+
+        //地球半径
         double R = 6371;
 
+        //两点间距离 km，如果想要米的话，结果*1000就可以了
         double d =  Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1))*R;
 
         return d;
-
     }
 
 
+    @Override
+    public void onMapClick(LatLng latLng) {
 
+        /*MarkerOptions markerOptions = new MarkerOptions();
+
+        markerOptions.position(latLng);
+        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+        mMap.clear();
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.addMarker(markerOptions);
+        */
+        alert = null;
+        builder = new AlertDialog.Builder(MapsActivity.this);
+        alert = builder
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setMessage("You are "+getDistance(start,latLng)+"km away")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        start = new LatLng(latLng.latitude, latLng.longitude);
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(latLng);
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                        mapFragment.googleMap.clear();
+                        mapFragment.googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                        mapFragment.googleMap.addMarker(markerOptions);
+                        /*streetViewPanoramaFragment.getStreetViewPanoramaAsync(
+                                new OnStreetViewPanoramaReadyCallback() {
+                                    @Override
+                                    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+                                        panorama.setPosition(latLng);
+
+                                    }
+                                });
+                         */
+                    }
+                })
+
+                .create();             //创建AlertDialog对象
+        alert.show();
+
+
+    }
 }
